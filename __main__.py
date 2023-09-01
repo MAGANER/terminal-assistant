cb1862273c3eee8aaad1a54c5a26f48c716c7d18
MAGANER <maganer.krik@mail.ru>
2023-09-01 23:15:09 +0300

    run window

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
-
-
+from window import Window
+from Perplexity import create_perplexity
 
 if __name__ == "__main__":
-    pass
+    win = Window(["llama-2-13b-sft","llama-2-70b-chat","llama-2-13b-chat","llama-2-7b-chat"])
+    win.run()
