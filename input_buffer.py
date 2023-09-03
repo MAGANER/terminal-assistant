abbeb65fa0ab47ffc18f925c40e5d8a2bc961a74
MAGANER <maganer.krik@mail.ru>
2023-09-03 15:02:49 +0300

    adopted it for better mode

--- input_buffer.py
+++ input_buffer.py
@@ -6,26 +6,39 @@ class InputBuffer:
         self.size = buffer_size
         self.screen = screen
 
-    def read(self,x,y):
+        self.quit_counter = 0
+        self.x = 7
+        self.y = 0
+
+    def __decr_q_counter(self):
+        if self.quit_counter != 0:
+            self.quit_counter-=1
+    def __is_enter(self,code):
+        return code == curses.KEY_ENTER or code == 10 or code == 13
+    
+    def read(self):
         code = -1
         self.__buffer = ""
 
         curses.echo()
-        self.screen.move(y,x)
+        self.screen.move(self.y,self.x)
         curses.curs_set(1)
-        while code != 10 and self.size != len(self.__buffer):
+        while self.size != len(self.__buffer):
             code = self.screen.getch()
-            
-            if code != 8:
+            self.x+=1
+            if code != 8 and not self.__is_enter(code):
                 self.__buffer+=chr(code)
-            elif code == 8:
+                self.__decr_q_counter()
+            elif code == 8 and not self.__is_enter(code):
                 self.__buffer = self.__buffer[:-1]
-                self.screen.addstr(y,x+len(self.__buffer)," ")
+                self.screen.addstr(self.y,self.x+len(self.__buffer)," ")
+                self.__decr_q_counter()
+            elif code == curses.KEY_ENTER or code == 10 or code == 13:
+                self.y+=1
+                self.quit_counter+=1
 
-                #new_x = 1 if len(self.__buffer) == 0 else x+len(self.__buffer)-1
-                #self.screen.move(y,new_x)
-
-        self.screen.addstr(y,x," "*len(self.__buffer))
-        curses.noecho()
-        curses.curs_set(0)
+            if self.quit_counter == 2:
+                self.x = 7
+                break
+                
         return self.__buffer
