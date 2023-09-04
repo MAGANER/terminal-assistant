af8658c897244744b1f1e4a9ba9e89b7398b5f2a
MAGANER <maganer.krik@mail.ru>
2023-09-04 19:39:43 +0300

    run chat menu

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
-from window import Window
-from Perplexity import create_perplexity
-
+from Chat import Chat
+import os
 if __name__ == "__main__":
-    win = Window(["llama-2-13b-sft","llama-2-70b-chat","llama-2-13b-chat","llama-2-7b-chat"])
-    win.run()
+    os.system("clear||cls")
+    
+    chat = Chat()
+    chat.run()
