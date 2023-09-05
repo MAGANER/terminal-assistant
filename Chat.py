050918ecde4f578bda6c9404fa3939d25a67128d
MAGANER <maganer.krik@mail.ru>
2023-09-05 23:42:01 +0300

    don't use curses

--- Chat.py
+++ Chat.py
@@ -1,86 +1,40 @@
-import curses
 from Perplexity import create_perplexity
-from input_buffer import InputBuffer
+from colors import color
+import os
+import time
+import keyboard
+
 class Chat:
     def __init__(self):
         self.__models = ["llama-2-13b-sft","llama-2-70b-chat","llama-2-13b-chat","llama-2-7b-chat"]
+        
         self.__curr_m = 0
-        
-        self.screen = curses.initscr()
-        self.screen.keypad(True)
-        
-        curses.raw()
-        curses.curs_set(0)
-
-        curses.start_color()
-        curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
-        curses.init_pair(2,curses.COLOR_WHITE,curses.COLOR_BLACK)
-        curses.init_pair(3,curses.COLOR_MAGENTA,curses.COLOR_BLACK)
-
         self.perp = None
 
-        self.buffer = InputBuffer(self.screen,500)
-        self.end_dialog = False
-        
-        
-    def __del__(self):
-        curses.endwin()
-        curses.curs_set(1)
-        self.screen.keypad(False)
-
-    ##these function are related to process os choosing of model. first phase of application using
-    def __print_models(self):
-        self.screen.addstr(0,0,"choose available model:")
+    def print_main_menu(self):
+        print("choose model to use:")
         for m in enumerate(self.__models):
             id, line = m
             if id == self.__curr_m:
-                self.screen.addstr(1+id,0,line,curses.color_pair(1))
+                print(color.yellow+line+color.end)
             else:
-                self.screen.addstr(1+id,0,line,curses.color_pair(2))
-    def __process_keys_to_choose_model(self):
-        val = self.screen.getch()
-        if val == curses.KEY_DOWN and self.__curr_m < 3:
-            self.__curr_m+=1
-        elif val == curses.KEY_DOWN and self.__curr_m == 3:
-            self.__curr_m = 0
-
-        if val == curses.KEY_UP and self.__curr_m > 0:
-            self.__curr_m-=1
-        elif val == curses.KEY_UP and self.__curr_m == 0:
-            self.__curr_m=3
-
-        if val == curses.KEY_ENTER or val == 10 or val == 13:
+                print(line)
+        os.system("clear|cls")
+    def process_keys(self):
+        if keyboard.is_pressed("up arrow"):
+            self.__curr_m = 3 if self.__curr_m == 0 else self.__curr_m - 1
+        if keyboard.is_pressed("down arrow"):
+            self.__curr_m = 0 if self.__curr_m == 3 else self.__curr_m + 1
+        if keyboard.is_pressed("enter"):
             return True
-            
-    def __choose_model(self):
-        while True:
-            self.__print_models()
-            if self.__process_keys_to_choose_model():
-                break
-            
-            self.screen.refresh()
-    ##################################
-
-    #these functions are related to communication with AI
-    def __run_prompt_input(self):
-        y,x = self.screen.getyx()
-        self.screen.addstr(y,x,"[you]:",curses.color_pair(3))
-        self.screen.move(y+1,x)
-        
-        s = self.buffer.read()
-        if s == -1:
-            self.end_dialog = True
-            
-        
-    def __talk_to_ai(self):
-        while not self.end_dialog:
-            self.__run_prompt_input()
-            self.screen.refresh()
-            
-    
+    def run_chat(self):
+        print(color.blue+"[you]:"+color.end,end="")
+        s = input()
     def run(self):
-        self.__choose_model()
-        self.perp = create_perplexity(self.__models[self.__curr_m])
+        while True:
+            self.print_main_menu()
+            if self.process_keys():
+                break
 
-        self.screen.clear()
-        self.__talk_to_ai()
+        while True:
+            self.run_chat()
--- __main__.py
+++ __main__.py
@@ -2,6 +2,8 @@ from Chat import Chat
 import os
 if __name__ == "__main__":
     os.system("clear||cls")
+    print('\033[?25l', end="")#hide cursor
     
     chat = Chat()
     chat.run()
+    print("\033[?25h",end="")
