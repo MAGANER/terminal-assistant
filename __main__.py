1f790a1a804005500d586e1af5c014236fb43ec9
MAGANER <maganer.krik@mail.ru>
2023-09-06 10:12:42 +0300

    run chat, init perplexity

--- __main__.py
+++ __main__.py
@@ -2,8 +2,12 @@ from Chat import Chat
 import os
 if __name__ == "__main__":
     os.system("clear||cls")
-    print('\033[?25l', end="")#hide cursor
-    
+   
     chat = Chat()
-    chat.run()
-    print("\033[?25h",end="")
+
+    try:
+        chat.run()
+    except Exception as e:
+        print(str(e))
+        print("due to exception program was shut down.")
+        exit(0)
