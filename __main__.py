0f6d24998324ebf712f5c56c70eb375f5db799ec
MAGANER <maganer.krik@mail.ru>
2023-09-06 19:16:42 +0300

    erased try-catch block, since it's not required anymore

--- __main__.py
+++ __main__.py
@@ -4,10 +4,4 @@ if __name__ == "__main__":
     os.system("clear||cls")
    
     chat = Chat()
-
-    try:
-        chat.run()
-    except Exception as e:
-        print(str(e))
-        print("due to exception program was shut down.")
-        exit(0)
+    chat.run()
